9f859789f5831a733b393b69dca3c93c637e853b
Wesley Teixeira <contato@wesleyteixeira.com.br>
2022-05-15 11:58:38 -0300

    layout cabeçalho com responsive fontsize

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
-import { Container, Title } from './styles'
+import { 
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+} from './styles'
 
 export function Dashboard(){
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{uri: 'https://avatars.githubusercontent.com/u/8378135?s=400&u=76ea444b45fd76e8dd0a1a9c659eea60073e592d&v=4'}}/>
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Wesley</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
