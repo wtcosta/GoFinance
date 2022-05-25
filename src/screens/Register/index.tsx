af228a096f165f494cef57dab62ee191df8a84f8
Wesley Teixeira <contato@wesleyteixeira.com.br>
2022-05-25 15:55:03 -0300

    salvando transsaction async storage

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -8,6 +8,9 @@ import {
 import { useForm } from 'react-hook-form'
 import * as Yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import uuid from 'react-native-uuid'
+import { useNavigation } from '@react-navigation/native'
 
 import { InputForm } from '../../components/Form/InputForm'
 import { Button } from '../../components/Form/Button'
@@ -44,14 +47,18 @@ export function Register(){
         key: 'category',
         name: 'Categoria'
     })
+
+    const dataKey = '@gofinances:transactions'
+    const navigation = useNavigation()
     
     const {
         control,
         handleSubmit,
+        reset,
         formState: {errors}
     } = useForm({resolver: yupResolver(schema)});
 
-    function handleRegister(form: FormData){
+    async function handleRegister(form: FormData){
         if (!transactionType) {
             return Alert.alert('Selecione o tipo da transação')
         }
@@ -60,13 +67,38 @@ export function Register(){
             return Alert.alert('Selecione a categoria')
         }
 
-        const data = {
+        const newTransaction = {
+            id: String(uuid.v4()),
             name: form.name,
             amount: form.amount,
             transactionType,
-            category: category.key
+            category: category.key,
+            date: new Date()
         }
-        console.log(data)
+        
+        try {
+            const data = await AsyncStorage.getItem(dataKey)
+            const currentData = data ? JSON.parse(data) : []
+
+            const dataFormatted = [
+                ...currentData,
+                newTransaction
+            ]
+
+            await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted))
+
+            reset()
+            setTransactionType('')
+            setCategory({
+                key: 'category',
+                name: 'Categoria'
+            })
+            navigation.navigate('Listagem')
+
+        } catch (error) {
+            console.error(error)
+            Alert.alert('Não foi possível salvar')
+        }
     }
 
     function handleTransactionTypeSelect(type: 'up' | 'down') {
