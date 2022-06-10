b6e254a47f133172a22c80576cf612b1c9c70118
Wesley Teixeira <contato@wesleyteixeira.com.br>
2022-06-09 21:09:43 -0300

    add listagem de categ pg resumo

---
+++ src/components/HistoryCard/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import {
+    Container,
+    Title,
+    Amount
+} from './style'
+
+interface Props {
+    title: string,
+    amount: string,
+    color: string
+}
+
+export function HistoryCard({
+    color, title, amount
+} : Props) {
+    return (
+        <Container color={color}>
+            <Title>{title}</Title>
+            <Amount>{amount}</Amount>
+        </Container>
+    )
+}
